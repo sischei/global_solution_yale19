0a8f71692c4d6fd9655e484cbd2e9b4fa39583ec
simon <simon.scheidegger@gmail.com>
2019-01-08 21:14:55 +0100

    updated files

--- Lecture_2/SparseGridCode/pyipopt_grace/setup_env.sh
+++
@@ -1,7 +0,0 @@
-# Set up the environment
-module load python/2.7-2015q2
-
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
-export IPOPT_DIR="`pwd`/Ipopt-3.12.5/build"
-export LD_LIBRARY_PATH=$IPOPT_DIR/lib:$LD_LIBRARY_PATH
-
