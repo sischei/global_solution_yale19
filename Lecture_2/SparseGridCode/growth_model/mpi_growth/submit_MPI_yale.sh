ea1404b9f8bdf635fccc9614a5a27449a6d0948a
Simon Scheidegger of group tsmith <ss3835@grace2.grace.hpc.yale.internal>
2019-01-17 16:05:40 -0500

    added submit

--- Lecture_2/SparseGridCode/growth_model/mpi_growth/submit_MPI_yale.sh
+++ Lecture_2/SparseGridCode/growth_model/mpi_growth/submit_MPI_yale.sh
@@ -6,9 +6,11 @@
 #SBATCH --time=00:10:00
 
 #make MPI available
-module load Langs/Python
-module load MPI/OpenMPI
-module load Libs/NUMPY
-module load Libs/MPI4PY
+module load Langs/Python/2.7.15-anaconda
+
+#module load Langs/Python
+#module load MPI/OpenMPI
+#module load Libs/NUMPY
+#module load Libs/MPI4PY
 
 mpirun python main.py
