a67de80bfa63085ad2307ece497b1ef04bfda7c7
iciac <47710784+iciac@users.noreply.github.com>
2021-11-04 15:29:52 +1000

    Update nonlinear_solver_iterate.py
    update to cyipopt

--- Lecture_6/code/growth_model_GPR/nonlinear_solver_iterate.py
+++ Lecture_6/code/growth_model_GPR/nonlinear_solver_iterate.py
@@ -5,12 +5,70 @@
 #     during the VFI.
 #
 #     Simon Scheidegger, 11/16 ; 07/17; 01/19
+
+#     Cameron Gordon, updates to Python3 11/21 
+#     Main difference is the shift from pyipopt to cyipopt 
+#     Involves a class to pass the optimisation problem to ipopt 
 #======================================================================
 
 from parameters import *
 from ipopt_wrapper import EV_F_ITER, EV_GRAD_F_ITER, EV_G_ITER, EV_JAC_G_ITER
 import numpy as np
-import pyipopt
+#import pyipopt 
+import cyipopt 
+
+class HS071(): 
+    """
+    Class for the optimization problem to be passed to cyipopt 
+    Further optimisations may be possible here by including a hessian (optional param) 
+    Uses the existing instance of the Gaussian Process (GP OLD) 
+    """
+
+    def __init__(self, X, n_agents, k_init, NELE_JAC, NELE_HESS, gp_old): 
+        self.x = X 
+        self.n_agents = n_agents 
+        self.k_init = k_init 
+        self.NELE_JAC = NELE_JAC 
+        self.NELE_HESS = NELE_HESS 
+        self.gp_old = gp_old 
+
+    # Create ev_f, eval_f, eval_grad_f, eval_g, eval_jac_g for given k_init and n_agent 
+    def eval_f(self, x): 
+        return EV_F_ITER(x, self.k_init, self.n_agents, self.gp_old) 
+
+    def eval_grad_f(self, x): 
+        return EV_GRAD_F_ITER(x, self.k_init, self.n_agents, self.gp_old) 
+
+    def eval_g(self, x): 
+        return EV_G_ITER(x, self.k_init, self.n_agents) 
+
+    def eval_jac_g(self, x, flag): 
+        return EV_JAC_G(x, flag, self.k_init, self.n_agents) 
+
+    def objective(self, x): 
+        # Returns the scalar value of the objective given x. 
+        return self.eval_f(x) 
+
+    def gradient(self, x): 
+        # Returns the gradient fo the objective with respect to x.""" 
+        return self.eval_grad_f(x) 
+
+    def constraints(self, x): 
+        # Returns the constraints 
+        return self.eval_g(x) 
+
+    def jacobian(self, x): 
+        # Returns the Jacobian of the constraints with respect to x. 
+        return self.eval_jac_g(x, False) 
+
+    def intermediate(self, alg_mod, iter_count, obj_value, inf_pr, inf_du, mu,
+                     d_norm, regularization_size, alpha_du, alpha_pr,
+                     ls_trials):
+        """Prints information at every Ipopt iteration."""
+
+        msg = "Objective value at iteration #{:d} is - {:g}"
+
+        print(msg.format(iter_count, obj_value))    
 
 def iterate(k_init, n_agents, gp_old):
     
@@ -70,6 +128,8 @@ def iterate(k_init, n_agents, gp_old):
     X[n_agents:2*n_agents]=lab_init
     X[2*n_agents:3*n_agents]=inv_init
     
+    """ 
+    Superseded by cyipopt 
     # Create ev_f, eval_f, eval_grad_f, eval_g, eval_jac_g for given k_init and n_agent 
         
     def eval_f(X):
@@ -83,7 +143,11 @@ def iterate(k_init, n_agents, gp_old):
         
     def eval_jac_g(X, flag):
         return EV_JAC_G_ITER(X, flag, k_init, n_agents)
-        
+    """
+
+    HS07 = HS071(X, n_agents, k_init, NELE_JAC, NELE_HESS, gp_old) # creates an instance of the class
+
+    """
     # First create a handle for the Ipopt problem 
     nlp=pyipopt.create(N, X_L, X_U, M, G_L, G_U, NELE_JAC, NELE_HESS, eval_f, eval_grad_f, eval_g, eval_jac_g)
     nlp.num_option("obj_scaling_factor", -1.0)
@@ -94,7 +158,24 @@ def iterate(k_init, n_agents, gp_old):
     nlp.int_option("print_level", 1)
     
     x, z_l, z_u, constraint_multipliers, obj, status=nlp.solve(X)
+
+    """
+    nlp=cyipopt.Problem(n=N, m = M, problem_obj=HS07, lb=X_L, ub=X_U, cl=G_L, cu=G_U,) 
+
+    nlp.addOption("obj_scaling_factor", -1.00) # max function 
+    nlp.addOption('mu_strategy', 'adaptive')
+    nlp.addOption('tol', 1e-6)
+    nlp.addOption("print_level", 0)
+    nlp.addOption("hessian_approximation", "limited-memory")
+
+    optimal_soln, info = nlp.solve(X)
+
+    x = info['x'] # soln of the primal variables 
+    g = info['g'] # constraint multipliers 
+    obj = info['obj_val'] #objective value 
+
     nlp.close()
+
     # x: Solution of the primal variables
     # z_l, z_u: Solution of the bound multipliers
     # constraint_multipliers: Solution of the constraint multipliers
